8a8e365b05718bd81a8c7ada7a222586ae992276
CyberBow <rizksalimrizk@gmail.com>
2023-06-14 01:08:34 +0300

    adding frames

--- webdirect.py
+++ webdirect.py
@@ -8,19 +8,41 @@ icon_path = "images/webdirectlogo.ico"
 plusImg = CTkImage(Image.open(r"images/plusimage.png"))
 
 
-def add_clicked():
-    print("clicked")
+# def add_clicked():
+#     print("add clicked")
+
+
+def delete_clicked():
+    print("delete clicked")
+
+
+class EntryFrame(CTkFrame):
+    def __int__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        site = CTkEntry(master=self)
+        site.grid(row=0, column=0, text="site")
+
+        redirect_website = CTkEntry(master=self)
+        redirect_website.grid(row=0, column=1, text="redirect website")
+
+        deleteButton = CTkButton(master=self, text="delete", command=delete_clicked)
+        deleteButton.grid(row=0, column=2)
 
 
 class WebFrame(CTkFrame):
     def __int__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
+    @staticmethod
+    def add_clicked(self):
+        entry = EntryFrame()
+        entry.grid(row=0, column=0)
+
 
 class TopFrame(CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-        add_button = CTkButton(master=self, text="ADD", image=plusImg, command=add_clicked)
+        add_button = CTkButton(master=self, text="ADD", image=plusImg, command=WebFrame.add_clicked)
         web_label = CTkLabel(master=self, text="website", padx=50)
         redirect_label = CTkLabel(master=self, text="redirect website")
 
