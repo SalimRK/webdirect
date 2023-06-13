bd8be078ebdd9ead34cfce7556f580b40d3c4d4e
CyberBow <rizksalimrizk@gmail.com>
2023-06-14 00:08:29 +0300

    frame, button click

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import tkinter
 from customtkinter import *
+from PIL import Image, ImageTk
 
 x = 800
 y = 500
-icon_path = "webdirectlogo.ico"
+icon_path = "images/webdirectlogo.ico"
+plusImg = CTkImage(Image.open(r"images/plusimage.png"))
 
-class redirect_sites(CTk):
-    pass
+
+class Entry(CTk):
+    def __int__(self, site, redirect_website, isOn):
+        self.site = site
+        self.redirect_website = redirect_website
+        self.isOn = isOn
+
+    def delete(self):
+        pass
 
 
 def add_clicked():
-    print("add button clicked")
+    entry_frame = CTkFrame(web_frame)
+    entry1 = CTkEntry(entry_frame)
+    entry2 = CTkEntry(entry_frame)
+    entry3 = CTkEntry(entry_frame)
+
+    entry_frame.grid(sticky="we")
+    entry1.grid(row=0, column=0)
+    entry2.grid(row=0, column=1, padx=10)
+    entry3.grid(row=0, column=2)
 
 
 if __name__ == '__main__':
@@ -21,26 +38,27 @@ if __name__ == '__main__':
     root.title("webDirect")
     root.geometry(f"{x}x{y}")
 
-
     # Frames
-    top_frame = CTkFrame(root, width=x, height=50, border_width=5)
+    top_frame = CTkFrame(root, width=x, height=100)
     web_frame = CTkFrame(root, width=x, height=y - 70, border_width=5)
 
     # Button
-    add_button = CTkButton(top_frame, text="ADD", command=add_clicked)
+    add_button = CTkButton(top_frame, text="ADD", image=plusImg, command=add_clicked)
 
     # labels
-    web_label = CTkLabel(top_frame, text="website")
+    web_label = CTkLabel(top_frame, text="website", padx=50)
     redirect_label = CTkLabel(top_frame, text="redirect website")
 
     # Grids
     # root
-    top_frame.grid(row=0, column=0, pady=10)
+    top_frame.grid(row=0, column=0, pady=10, sticky="we")
     web_frame.grid(row=1, column=0)
+
     # top_frame
-    # web_label.grid(row=0, column=0)
-    # redirect_label.grid(row=0, column=1)
-    # add_button.grid(row=0, column=2)
+    web_label.grid(row=0, column=0)
+    redirect_label.grid(row=0, column=1, padx=x / 4)
+    add_button.grid(row=0, column=2)
+
     # web_frame
 
     root.mainloop()
