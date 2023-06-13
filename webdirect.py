5d33a317f0898324a06693d98601a4f152f5ca2f
CyberBow <rizksalimrizk@gmail.com>
2023-06-14 01:47:28 +0300

    add button and delete

--- webdirect.py
+++ webdirect.py
@@ -8,47 +8,47 @@ icon_path = "images/webdirectlogo.ico"
 plusImg = CTkImage(Image.open(r"images/plusimage.png"))
 
 
-# def add_clicked():
-#     print("add clicked")
-
-
-def delete_clicked():
-    print("delete clicked")
-
-
 class EntryFrame(CTkFrame):
-    def __int__(self, master, **kwargs):
+    def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.master = master
         site = CTkEntry(master=self)
-        site.grid(row=0, column=0, text="site")
+        site.grid(row=0, column=0)
 
         redirect_website = CTkEntry(master=self)
-        redirect_website.grid(row=0, column=1, text="redirect website")
+        redirect_website.grid(row=0, column=1, padx=x / 4)
 
-        deleteButton = CTkButton(master=self, text="delete", command=delete_clicked)
+        deleteButton = CTkButton(master=self, text="delete", command=self.delete_clicked)
         deleteButton.grid(row=0, column=2)
+
+    def delete_clicked(self):
+        self.destroy()
 
 
 class WebFrame(CTkFrame):
-    def __int__(self, master, **kwargs):
+    def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.master = master
 
-    @staticmethod
     def add_clicked(self):
-        entry = EntryFrame()
-        entry.grid(row=0, column=0)
+        entry = EntryFrame(self)
+        entry.pack()
 
 
 class TopFrame(CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-        add_button = CTkButton(master=self, text="ADD", image=plusImg, command=WebFrame.add_clicked)
+        self.master = master
+        add_button = CTkButton(master=self, text="ADD", image=plusImg, command=self.add_clicked)
         web_label = CTkLabel(master=self, text="website", padx=50)
         redirect_label = CTkLabel(master=self, text="redirect website")
 
         web_label.grid(row=0, column=0)
         redirect_label.grid(row=0, column=1, padx=x / 4)
         add_button.grid(row=0, column=2)
+
+    def add_clicked(self):
+        self.master.web_frame.add_clicked()
 
 
 class App(CTk):
@@ -58,10 +58,10 @@ class App(CTk):
         self.title("webDirect")
         self.geometry(f"{x}x{y}")
 
-        top_frame = TopFrame(master=self)
-        top_frame.grid(row=0, column=0, pady=10, sticky="we")
-        web_frame = WebFrame(master=self, width=x, height=y - 70, border_width=5)
-        web_frame.grid(row=1, column=0)
+        self.top_frame = TopFrame(master=self)
+        self.top_frame.grid(row=0, column=0, pady=10, sticky="we")
+        self.web_frame = WebFrame(master=self, width=x, height=y - 70, border_width=5)
+        self.web_frame.grid(row=1, column=0)
 
 
 if __name__ == '__main__':
